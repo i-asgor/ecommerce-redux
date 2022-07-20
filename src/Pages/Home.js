1999ab9b65216637622a600ae78a88b948762bfe
Md Asgor Office <asgor.ice@gmail.com>
2022-07-20 13:27:54 +0600

    banner added

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,39 +2,18 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
-                <div class="carousel-indicators">
-                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                </div>
-                <div class="carousel-inner">
-                    <div class="carousel-item active" data-bs-interval="10000">
-                    <img src="/assets/slider/1.png" class="d-block w-100" alt="..." />
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>First slide label</h5>
-                        <p>Some representative placeholder content for the first slide.</p>
-                    </div>
-                    </div>
-                    <div class="carousel-item" data-bs-interval="2000">
-                    <img src="/assets/slider/2.png" class="d-block w-100" alt="..." />
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>Second slide label</h5>
-                        <p>Some representative placeholder content for the second slide.</p>
-                    </div>
-                    </div>
-                   
-                </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
-                </div>
+        <div className='hero'>
+            <div class="card text-light">
+            <img src="/assets/slider/2.png" class="card-img" alt="background" height="550px" />
+            <div class="card-img-overlay d-flex flex-column justify-content-center">
+                <div className="container">
+                    <h5 class="card-title display-2 fw-bolder">New Season</h5>
+                    <p class="card-text lead fs-2">
+                        Check out all the trends
+                    </p>
+                </div>                
+            </div>
+            </div>
         </div>
     );
 };
