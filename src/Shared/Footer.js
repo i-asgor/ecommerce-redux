c17e9eabcd3b0c744a42e07fee62ae7f0c0cd7c7
Md. Abdullah-Al-Asgor Ali <asgor.ice@gmail.com>
2022-07-20 20:31:58 +0600

    footer updated

--- src/Shared/Footer.js
+++ src/Shared/Footer.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { NavLink} from "react-router-dom";
 
 const Footer = () => {
     return (
         <div class="container">
         <footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
             <div class="col mb-3">
-            <a href="/" class="d-flex align-items-center mb-3 lead text-bolder text-dark text-decoration-none">
+            <NavLink to="/" class="d-flex align-items-center mb-3 lead text-bolder text-dark text-decoration-none">
             E-Commerce
 
-            </a>
+            </NavLink>
             <p class="text-muted">© 2022</p>
             </div>
 
@@ -18,31 +19,31 @@ const Footer = () => {
 
             <div class="col mb-3">
             <ul class="nav flex-column">
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+                <li class="nav-item mb-2"><NavLink to="#" class="nav-link p-0 text-muted">Home</NavLink></li>
+                <li class="nav-item mb-2"><NavLink to="#" class="nav-link p-0 text-muted">Features</NavLink></li>
+                <li class="nav-item mb-2"><NavLink to="#" class="nav-link p-0 text-muted">Pricing</NavLink></li>
+                <li class="nav-item mb-2"><NavLink to="#" class="nav-link p-0 text-muted">FAQs</NavLink></li>
+                <li class="nav-item mb-2"><NavLink to="#" class="nav-link p-0 text-muted">About</NavLink></li>
             </ul>
             </div>
 
             <div class="col mb-3">
             <ul class="nav flex-column">
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+                <li class="nav-item mb-2"><NavLink to="#" class="nav-link p-0 text-muted">Home</NavLink></li>
+                <li class="nav-item mb-2"><NavLink to="#" class="nav-link p-0 text-muted">Features</NavLink></li>
+                <li class="nav-item mb-2"><NavLink to="#" class="nav-link p-0 text-muted">Pricing</NavLink></li>
+                <li class="nav-item mb-2"><NavLink to="#" class="nav-link p-0 text-muted">FAQs</NavLink></li>
+                <li class="nav-item mb-2"><NavLink to="#" class="nav-link p-0 text-muted">About</NavLink></li>
             </ul>
             </div>
 
             <div class="col mb-3">
             <ul class="nav flex-column">
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+                <li class="nav-item mb-2"><NavLink to="#" class="nav-link p-0 text-muted">Home</NavLink></li>
+                <li class="nav-item mb-2"><NavLink to="#" class="nav-link p-0 text-muted">Features</NavLink></li>
+                <li class="nav-item mb-2"><NavLink to="#" class="nav-link p-0 text-muted">Pricing</NavLink></li>
+                <li class="nav-item mb-2"><NavLink to="#" class="nav-link p-0 text-muted">FAQs</NavLink></li>
+                <li class="nav-item mb-2"><NavLink to="#" class="nav-link p-0 text-muted">About</NavLink></li>
             </ul>
             </div>
         </footer>
