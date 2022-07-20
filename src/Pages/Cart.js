577588a3d0f24facfc3387812458e8d2aea2a477
Md Asgor Office <asgor.ice@gmail.com>
2022-07-20 18:08:33 +0600

    Product remove from cart in cart page

--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -1,16 +1,50 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { deleteCart } from '../redux/action';
 
 const Cart = () => {
-    const state = useSelector((state) => state.addItem);
+    const state = useSelector((state)=> state.handleCart)
     const dispatch = useDispatch();
 
-    const cartItems = (cartItem) => {
-        
+    const handleClose = (item) => {
+        dispatch(deleteCart(item))
     }
+
+    const cartItems = (cartItem) => {
+        return(
+            <div className='px-4 my-5 bg-light rounded-3'>
+                <div className="container">
+                    <button onClick={()=> handleClose(cartItem)} className="btn-close float-end" aria-label='Close'></button>
+                    <div className="row justify-content-center">
+                        <div className="col-md-4">
+                            <img src={cartItem.image} alt={cartItem.title} height="200px" width="180px" />
+                        </div>
+                        <div className="col-md-4">
+                            <h3>{cartItem.title}</h3>
+                            <p>{cartItem.price}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    const emptyCart = () => {
+        return(
+            <div className="px-4 my-5 bg-light rounded-3 py-5">
+            <div className="container py-4">
+                <div className="row">
+                    <h1>No Product Added in Your Cart</h1>
+                </div>
+            </div>
+        </div>
+        )
+    }
+
     return (
         <div>
-            product
+            {state.length === 0 && emptyCart()}
+            {state.length !==0 && state.map(cartItems)}
         </div>
     );
 };
